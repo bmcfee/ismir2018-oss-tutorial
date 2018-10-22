862a8a5d3007eabe1ba7b6f5a407ee99dae8588e
Sertan Senturk <contact@sertansenturk.com>
2018-10-22 23:44:07 +0100

    Updated .travis.yml

--- toymir/tests/test_toymir.py
+++ toymir/tests/test_toymir.py
@@ -6,26 +6,26 @@ import toymir
 
 def test_midi_to_hz_float():
     expected = 440.0
-    assert toymir.midi_to_hz(69) == expected
+    assert toymir.freq.midi_to_hz(69) == expected
 
 
 def test_midi_to_hz_array():
     expected = [261.6255653, 329.62755691, 440.0]
-    assert np.allclose(toymir.midi_to_hz([60, 64, 69]), expected)
+    assert np.allclose(toymir.freq.midi_to_hz([60, 64, 69]), expected)
 
 
 # Hello, you're probably at the start of Part 5!
 # These are the two tests you should uncomment!
 
 
-# def test_hz_to_midi_float():
-#     expected = 69
-#     assert toymir.hz_to_midi(440.0) == expected
+def test_hz_to_midi_float():
+    expected = 69
+    assert toymir.freq.hz_to_midi(440.0) == expected
 
 
-# def test_hz_to_midi_array():
-#     expected = [57, 69, 81]
-#    assert np.allclose(toymir.hz_to_midi([220.0, 440.0, 880.0]), expected)
+def test_hz_to_midi_array():
+    expected = [57, 69, 81]
+    assert np.allclose(toymir.freq.hz_to_midi([220.0, 440.0, 880.0]), expected)
 
 
 # Hello!  You could add the missing test for test_hz_to_midi here!
@@ -33,16 +33,16 @@ def test_midi_to_hz_array():
 
 def test_hz_to_period_float():
     expected = 0.1
-    assert toymir.hz_to_period(10) == expected
+    assert toymir.freq.hz_to_period(10) == expected
 
 
 def test_hz_to_period_array():
     expected = [0.1, 0.01, 0.001]
-    assert np.allclose(toymir.hz_to_period([10, 100, 1000]), expected)
+    assert np.allclose(toymir.freq.hz_to_period([10, 100, 1000]), expected)
 
 
 def test_hz_to_period_throws_if_zero_or_less():
     # This is a bit magic-looking, but all it is saying is that
     # the test will pass if code inside the `with` block raises a ValueError!
     with pytest.raises(ValueError):
-        toymir.hz_to_period(0)
+        toymir.freq.hz_to_period(0)
