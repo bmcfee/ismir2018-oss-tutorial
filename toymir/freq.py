d39d996059cd70383c79e0054425a4cb90f8c48f
Felicia <feliciavillalobos685@gmail.com>
2019-02-06 09:29:06 -0500

    fixed indent

--- toymir/freq.py
+++ toymir/freq.py
@@ -11,24 +11,20 @@ def midi_to_hz(notes):
 
 def hz_to_midi(frequencies):
     """Get MIDI note number(s) for given frequencies
-
     Parameters
     ----------
     frequencies   : float or np.ndarray [shape=(n,), dtype=float]
         frequencies to convert
-
     Returns
     -------
     note_nums     : number or np.ndarray [shape=(n,), dtype=float]
         MIDI notes to `frequencies`
-
     Examples
     --------
     >>> hz_to_midi(60)
     34.506
     >>> hz_to_midi([110, 220, 440])
     array([ 45.,  57.,  69.])
-
     See Also
     --------
     midi_to_hz
@@ -38,35 +34,30 @@ def hz_to_midi(frequencies):
     # Oh hey, it's Part 5!  You could uncomment this implementation,
     # and then the tests will pass!
 
-    # less_than_zero = (np.asanyarray(frequencies) <= 0).any()
+    less_than_zero = (np.asanyarray(frequencies) <= 0).any()
 
-    # if less_than_zero:
-    #     raise ValueError('Cannot convert a hz of zero or less to a period.')
+    if less_than_zero:
+         raise ValueError('Cannot convert a hz of zero or less to a period.')
 
-    # return 12 * (np.log2(np.asanyarray(frequencies)) - np.log2(440.0)) + 69
+    return 12 * (np.log2(np.asanyarray(frequencies)) - np.log2(440.0)) + 69
 
 
 def hz_to_period(frequencies):
     """Get the period of a frequency (Hz) in seconds.
-
     Parameters
     ----------
     frequencies   : float or np.ndarray [shape=(n,), dtype=float]
         frequencies to convert
-
     Returns
     -------
     period   : number or np.ndarray [shape=(n,), dtype=float]
         period (periods) of `frequencies` in seconds.
-
     Examples
     --------
     >>> hz_to_period(100)
     0.01
-
     >>> hz_to_period([110, 220, 440])
     array([0.00909091, 0.00454545, 0.0030303 ])
-
     See Also
     --------
     hz_to_midi
