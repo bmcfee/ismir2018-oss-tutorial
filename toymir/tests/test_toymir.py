b0f74c9ae00124ff49456772bed9cd6082c9b1a3
sashayeu <syeutseyeva@smith.edu>
2020-05-26 23:23:37 -0400

    fourth time?

--- toymir/tests/test_toymir.py
+++ toymir/tests/test_toymir.py
@@ -27,6 +27,7 @@ def test_hz_to_midi_array():
     expected = [57, 69, 81]
     assert np.allclose(toymir.hz_to_midi([220.0, 440.0, 880.0]), expected)
 
+
 def test_hz_to_period_throws_of_zero_or_less():
     with pytest.raises(ValueError):
         toymir.hz_to_period(0)
